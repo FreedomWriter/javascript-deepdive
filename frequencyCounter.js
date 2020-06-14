ebeeafa3cd38a0a5270b0dccb465b00788db3621
FreedomWriter <natalienetworks@gmail.com>
2020-06-13 21:53:46 -0500

    O(N) solution - linear

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -14,8 +14,8 @@ Write a function called same, which accepts two arrays. The function should retu
     same([1,2,3],[4,1,9]) //false (must be same frequency)
 */
 
-// Naive solution - O(N^2) (indexOf effectively loops)
-function same(arr1, arr2) {
+// Naive solution - O(N^2) (indexOf effectively (potentially) loops over entire array)
+function sameON2(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -33,6 +33,40 @@ function same(arr1, arr2) {
   return true;
 }
 
-console.log(same([1, 2, 3], [4, 1, 9]));
-console.log(same([1, 2, 3], [1, 9]));
-console.log(same([1, 2, 3], [4, 4, 9]));
+// console.log(sameON2([1, 2, 3], [4, 1, 9]));
+// console.log(sameON2([1, 2, 3], [1, 9]));
+// console.log(sameON2([1, 2, 3], [4, 4, 9]));
+
+// O(n) Solution
+
+/* 
+Two loops is better than 2 nested loops 
+- if N is 1,000 elements, 2 loops means 2,000 iterations (1000 for loop 1 + 1000 for loop 2)
+- 2 nested loops where N is 1,000 elements means 1,000,000 iterations ( 1,000 loops for the outer loop and 1,000 loops for each element in the loop because of the inner loop)
+*/
+function sameOn(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(sameOn([1, 2, 3], [4, 1, 9]));
+console.log(sameOn([1, 2, 3], [1, 9]));
+console.log(sameOn([1, 2, 3], [4, 4, 9]));
