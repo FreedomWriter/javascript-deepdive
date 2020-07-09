7bf8961f06d0f5f046c0ba3e0e6edbbe7cfa8d54
FreedomWriter <natalienetworks@gmail.com>
2020-07-08 20:24:51 -0500

    minimal ds notes

---
+++ data-structures.js
@@ -0,0 +1,21 @@
+/* 
+
+What is a Data Structure?
+    a collection of values that can have relationships among them and can have functions applied to them
+    each one is different in what it can do and what it is best used for
+    each one is specialized for it's own thing
+
+How do computers store data?
+    CPU
+        connected to the memory controlled which does the reading and writing to RAM
+        build in speed boosts to access things stored closer together in RAM
+        CPU cahce is a small amount of membory that the CPU has which stores a copy of very recient things
+    RAM
+        stores varaibles - not persistant
+        faster than storage
+        Stores things in `addresses` which hold 8 bits, which is one byte
+    Storage
+        references persitent memory
+        slower than RAM
+
+*/
