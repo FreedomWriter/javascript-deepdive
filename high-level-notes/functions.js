cfad0cf3808bf3087af637542ff62dceed148949
FreedomWriter <natalienetworks@gmail.com>
2020-10-07 19:02:54 -0500

    optimized solution to trapping rainwater

--- high-level-notes/functions.js
+++ high-level-notes/functions.js
@@ -1,114 +1,114 @@
-/*
-REVIEW
-------
+// /*
+// REVIEW
+// ------
 
-- When we invoke a function we are given 2 parameters automatically:
-    - this keyword
-    - arguments keyword
+// - When we invoke a function we are given 2 parameters automatically:
+//     - this keyword
+//     - arguments keyword
 
-- Arguments is an array like object that has some odd behaviors for looping and iteration
-    - because of the arguments object, we can technically not have any parameters defined for a function, pass the function parameters during invocation and grab them in the body of the function using the arguments keyword
+// - Arguments is an array like object that has some odd behaviors for looping and iteration
+//     - because of the arguments object, we can technically not have any parameters defined for a function, pass the function parameters during invocation and grab them in the body of the function using the arguments keyword
 
-- When we define fns and the compiler looks at our code, lexically it determines what variables are available and adds scope chains
+// - When we define fns and the compiler looks at our code, lexically it determines what variables are available and adds scope chains
 
-Functions are a first class citizen, what does this mean?
-1. Functions can be assigned to a variable
-2. Functions can be passed as arguments to a function
-3. Functions can be returned as values from another function
-*/
+// Functions are a first class citizen, what does this mean?
+// 1. Functions can be assigned to a variable
+// 2. Functions can be passed as arguments to a function
+// 3. Functions can be returned as values from another function
+// */
 
-// you can add properties to functions but WHY WOULD YOU DO THIS???
-function woho() {
-  console.log("whoohohoho");
-}
+// // you can add properties to functions but WHY WOULD YOU DO THIS???
+// function woho() {
+//   console.log("whoohohoho");
+// }
 
-woho.yell = "aaaahhhhhhhh";
+// woho.yell = "aaaahhhhhhhh";
 
-console.log(woho.yell);
+// console.log(woho.yell);
 
-// Things to watch out for.
+// // Things to watch out for.
 
-/* ********************************************************************************* */
-// INITALIZING IN LOOPS
+// /* ********************************************************************************* */
+// // INITALIZING IN LOOPS
 
-// DON'T
-for (let i = 0; i < 5; i++) {
-  // this means every iteration of the loop cause the function to be redeclared
-  function a() {
-    return "do stuff";
-  }
-}
+// // DON'T
+// for (let i = 0; i < 5; i++) {
+//   // this means every iteration of the loop cause the function to be redeclared
+//   function a() {
+//     return "do stuff";
+//   }
+// }
 
-//DO
-function a() {
-  return "do stuff";
-}
-for (let i = 0; i < 5; i++) {
-  a();
-}
-/* ********************************************************************************* */
+// //DO
+// function a() {
+//   return "do stuff";
+// }
+// for (let i = 0; i < 5; i++) {
+//   a();
+// }
+// /* ********************************************************************************* */
 
-// HOF
-// A function that takes a function as an argument or a function that returns a function
+// // HOF
+// // A function that takes a function as an argument or a function that returns a function
 
-function letMeLogin() {
-  let array = [];
-  for (let i = 0; i < 100000000; i++) {
-    array.push(i);
-  }
-  return "Access Granted to Me";
-}
-function letYouLogin() {
-  let array = [];
-  for (let i = 0; i < 100000000; i++) {
-    array.push(i);
-  }
-  return "Access Granted to You";
-}
+// function letMeLogin() {
+//   let array = [];
+//   for (let i = 0; i < 100000000; i++) {
+//     array.push(i);
+//   }
+//   return "Access Granted to Me";
+// }
+// function letYouLogin() {
+//   let array = [];
+//   for (let i = 0; i < 100000000; i++) {
+//     array.push(i);
+//   }
+//   return "Access Granted to You";
+// }
 
-const giveAccessTo = (name) => `Access granted to ${name}`;
+// const giveAccessTo = (name) => `Access granted to ${name}`;
 
-function letUserLogin(user) {
-  let array = [];
-  for (let i = 0; i < 100000000; i++) {
-    array.push(i);
-  }
-  return giveAccessTo(user);
-}
+// function letUserLogin(user) {
+//   let array = [];
+//   for (let i = 0; i < 100000000; i++) {
+//     array.push(i);
+//   }
+//   return giveAccessTo(user);
+// }
 
-function letAdminLogin(admin) {
-  let array = [];
-  for (let i = 0; i < 100000000; i++) {
-    array.push(i);
-  }
-  return giveAccessTo(admin);
-}
+// function letAdminLogin(admin) {
+//   let array = [];
+//   for (let i = 0; i < 100000000; i++) {
+//     array.push(i);
+//   }
+//   return giveAccessTo(admin);
+// }
 
-// refined version includes giveAccessTo from earlier version, as well as authenticate and letLogin
-function authenticate(verify) {
-  let array = [];
-  for (let i = 0; i < verify; i++) {
-    array.push(i);
-  }
-  return true;
-}
+// // refined version includes giveAccessTo from earlier version, as well as authenticate and letLogin
+// function authenticate(verify) {
+//   let array = [];
+//   for (let i = 0; i < verify; i++) {
+//     array.push(i);
+//   }
+//   return true;
+// }
 
-function letLogin(person, fn = authenticate) {
-  if (person.level === "admin") {
-    fn(100000000);
-  } else if (person.level === "user") {
-    console.log(fn(10000000));
-  }
-  return giveAccessTo(person.name);
-}
+// function letLogin(person, fn = authenticate) {
+//   if (person.level === "admin") {
+//     fn(100000000);
+//   } else if (person.level === "user") {
+//     console.log(fn(10000000));
+//   }
+//   return giveAccessTo(person.name);
+// }
 
-function sing(person) {
-  return "la la lala lalalalal " + person;
-}
-console.log(letMeLogin());
-console.log(letYouLogin());
-console.log(letUserLogin("Nosey"));
-console.log(letLogin({ name: "Natalie", level: "admin" }));
-console.log(letLogin({ name: "Not Natalie", level: "user" }));
-console.log(letLogin({ name: "Not Natalie", level: "user" }, sing));
-/* ********************************************************************************* */
+// function sing(person) {
+//   return "la la lala lalalalal " + person;
+// }
+// console.log(letMeLogin());
+// console.log(letYouLogin());
+// console.log(letUserLogin("Nosey"));
+// console.log(letLogin({ name: "Natalie", level: "admin" }));
+// console.log(letLogin({ name: "Not Natalie", level: "user" }));
+// console.log(letLogin({ name: "Not Natalie", level: "user" }, sing));
+// /* ********************************************************************************* */
--- ztm/trappingRainwater.js
+++ ztm/trappingRainwater.js
@@ -1,36 +1,103 @@
-// console.log("TRAPPING RAINWATER");
+console.log("TRAPPING RAINWATER");
 
-// /*
-// Given an arraw of integers representing an elevation map where the width of each bar is 1, return how much rainwater can be trapped
+/*
+Given an arraw of integers representing an elevation map where the width of each bar is 1, return how much rainwater can be trapped
 
-//  0  1  2  3  4  5  6  7  8  9  10
-// [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
+ 0  1  2  3  4  5  6  7  8  9  10
+[0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
 
-// STEP ONE - VERIFY THE CONSTRAINTS
-// ---------------------------------
+STEP ONE - VERIFY THE CONSTRAINTS
+---------------------------------
 
-// - Do the sides of the graph count as walls?
-//  - No
+- Do the sides of the graph count as walls?
+ - No
 
-// - Will there be negative integers?
-//     - No
+- Will there be negative integers?
+    - No
 
-// STEP TWO - WRITE OUT SOME TEST CASES
-// ------------------------------------
+STEP TWO - WRITE OUT SOME TEST CASES
+------------------------------------
 
-//  0  1  2  3  4  5  6  7  8  9  10
-// [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2] = 8
-// [] = 0
-// [3] = 0
-// [3,4,3] = 0
-// [5, 0, 3, 0, 0, 0, 2, 3, 4, 2, 1]
+ 0  1  2  3  4  5  6  7  8  9  10
+[0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2] = 8
+[] = 0
+[3] = 0
+[3,4,3] = 0
+[5, 0, 3, 0, 0, 0, 2, 3, 4, 2, 1]
 
-// STEP THREE - SOLVE WITHOUT CODE
-// -------------------------------
+STEP THREE - SOLVE WITHOUT CODE
+-------------------------------
 
-// currWater = min(maxL, maxR) - curHeight
-// */
+currWater = min(maxL, maxR) - curHeight
+*/
 
-// function trapping(arr) {
-//   console.log(arr);
-// }
+function trapping(heights) {
+  let totalWater = 0;
+  let leftP, rightP, maxLeft, maxRight;
+
+  for (let p = 0; p < heights.length; p++) {
+    leftP = p;
+    rightP = p;
+    maxLeft = 0;
+    maxRight = 0;
+
+    while (leftP >= 0) {
+      maxLeft = Math.max(maxLeft, heights[leftP]);
+      leftP--;
+    }
+
+    while (rightP < heights.length) {
+      maxRight = Math.max(maxRight, heights[rightP]);
+      rightP++;
+    }
+
+    const curWater = Math.min(maxLeft, maxRight) - heights[p];
+
+    if (curWater >= 0) {
+      totalWater += curWater;
+    }
+  }
+
+  return totalWater;
+}
+
+console.log(trapping([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]));
+console.log(trapping([]));
+console.log(trapping([3]));
+console.log(trapping([3, 4, 3]));
+console.log(trapping([5, 0, 3, 0, 0, 0, 2, 3, 4, 2, 1]));
+
+function trappingOptimized(heights) {
+  let left = 0,
+    right = heights.length - 1,
+    totalWater = 0,
+    maxLeft = 0,
+    maxRight = 0;
+
+  while (left < right) {
+    if (heights[left] <= heights[right]) {
+      if (heights[left] >= maxLeft) {
+        maxLeft = heights[left];
+      } else {
+        totalWater += maxLeft - heights[left];
+      }
+      left++;
+    } else {
+      if (heights[right] >= maxRight) {
+        maxRight = heights[right];
+      } else {
+        totalWater += maxRight - heights[right];
+      }
+
+      right--;
+    }
+  }
+
+  return totalWater;
+}
+
+console.log(trappingOptimized([0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]));
+console.log(trappingOptimized([]));
+console.log(trappingOptimized([3]));
+console.log(trappingOptimized([3, 4, 3]));
+console.log(trappingOptimized([5, 0, 3, 0, 0, 0, 2, 3, 4, 2, 1]));
