b3d5735349b6098916f081ebcb6daa9184f5cc3b
FreedomWriter <natalienetworks@gmail.com>
2020-10-12 15:23:34 -0500

    constructor functions

---
+++ high-level-notes/oop.js
@@ -0,0 +1,114 @@
+console.log("OOP");
+/*
+In Object Oriented Programming (oop) an object is a box contianing information and operations that refer to the same concept
+
+In oop the data can be refered to as state
+
+2 main types:
+- Prototype based 
+- Class based
+*/
+
+/* Factory Functions */
+
+// naive approach - create an object
+// we would have to copy/paste to create new elf's
+// we do have encapsulation
+// we have state and methods acting on that state
+
+const naiveElf = {
+  name: "Naive Elf",
+  weapon: "bow",
+  attack() {
+    return "attack with " + naiveElf.weapon;
+  },
+};
+
+// console.log(naiveElf.name);
+// console.log(naiveElf.attack());
+
+// Factory function - a function that creates objects
+// we've avoided repetitive code
+// this isn't the most memory performant
+// instead of one instance of attack being stored in memory and used by each new instance of elf created by the function, attack is copied and stored for each instance of elf - if we had a billion elves (as one does) this would get costly
+
+function createElf(name, weapon) {
+  return {
+    name,
+    weapon,
+    attack() {
+      return "attack with " + weapon;
+    },
+  };
+}
+
+const factoryElf = createElf("Peter", "stones");
+
+// console.log(factoryElf.name);
+// console.log(factoryElf.attack());
+
+// naive approach to solving memory problem
+// this method allows us to have an oject of methods that can be given to elves.
+// the methods are only defined and stored once in memory and then extended to elves
+const elfFunctions = {
+  attack(weapon) {
+    return "elfFunction attack with " + weapon;
+  },
+};
+
+function createElf2(name, weapon) {
+  return {
+    name,
+    weapon,
+  };
+}
+
+const elfFunction = createElf2("Elf Function", "Dry Code");
+
+elfFunction.attack = elfFunctions.attack;
+// console.log(elfFunction.attack(elfFunction.weapon));
+
+// The above approach can be improved with Object.create
+// this approach creates a definitive link between elfFunctionStore and newElf created by createElf3
+// this is an example of true prototypal inheritance, however this approach is not standard or widely accepted by the js community
+// this wouldn't actually be considered OOP
+const elfFunctions2 = {
+  attack(weapon) {
+    return "elfFunction attack with " + weapon;
+  },
+};
+
+function createElf3(name, weapon) {
+  let newElf = Object.create(elfFunctions2);
+
+  newElf.name = name;
+  newElf.weapon = weapon;
+
+  return newElf;
+}
+
+const elfFunctionDryer = createElf3("Elf Function2", "Dryer Code");
+
+// console.log(elfFunctionDryer.attack(elfFunctionDryer.weapon));
+
+/*
+Constructor Functions
+
+requires the `new` keyword
+convention dictates that constructor functions begin with a capital letter as a signal to other devs that this is a constructor function that requires the use of the new keyword
+
+*/
+
+function ConstructorElf(name, weapon) {
+  this.name = name;
+  this.weapon = weapon;
+}
+
+ConstructorElf.prototype.attack = function () {
+  return "Constructor attack: " + this.weapon;
+};
+
+const bob = new ConstructorElf("Bob", "Constructor weapon");
+console.log(bob.name);
+console.log(bob.weapon);
+console.log(bob.attack());
