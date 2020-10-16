13e08ba27b89c6b3051f07c9faeb255f9438b274
FreedomWriter <natalienetworks@gmail.com>
2020-10-16 13:39:22 -0500

    try/catch/finally

--- high-level-notes/modules-in-js.js
+++ high-level-notes/modules-in-js.js
@@ -1,58 +1,60 @@
-console.log("MODULES");
+// console.log("MODULES");
 
-/*
-What are modules?
+// /*
+// What are modules?
 
-*/
+// */
 
-// tight coupling means everything is connected really tightly
+// // tight coupling means everything is connected really tightly
 
-//Module Pattern
+// //Module Pattern
 
-const fightModule = (function () {
-  const harry = "potter";
-  const voldermort = "evil";
+// const fightModule = (function () {
+//   const harry = "potter";
+//   const voldermort = "evil";
 
-  function fight(char1 = harry, char2 = voldermort) {
-    let attack1 = Math.floor(Math.random() * char1.length);
-    let attack2 = Math.floor(Math.random() * char2.length);
-    return attack1 > attack2 ? `${char1} wins` : `${char2} wins`;
-  }
-  //revealing module pattern
-  return {
-    fight: fight,
-  };
-})();
-
-console.log(fightModule.fight("natalie", "no"));
-console.log(fightModule.fight());
-
-/*
-CommonJS and AMD (asynchronous module definition)
-this is what I'm familiar with through node
-*/
-
-// const api = require("api");
-
-// function someFunc() {}
-
-// module.exports = {
-//   someFunc,
-// };
-
-// Modules are meant to be loaded synchronously- which works fine in node but not ideal for browser use
-
-// AMD looks like this:
-// designed specifically for the browser to load scripts or modules asynchronously
-
-// define(["module1", "module2"], function (module1Import, module2Import) {
-//   let module1 = module1Import; //.fight;
-//   let module2 = module2Import; // .importedFunc2
-
-//   function dance() {
-//     //do stuff
+//   function fight(char1 = harry, char2 = voldermort) {
+//     let attack1 = Math.floor(Math.random() * char1.length);
+//     let attack2 = Math.floor(Math.random() * char2.length);
+//     return attack1 > attack2 ? `${char1} wins` : `${char2} wins`;
 //   }
+//   //revealing module pattern
 //   return {
-//     dance: dance,
+//     fight: fight,
 //   };
-// });
+// })();
+
+// console.log(fightModule.fight("natalie", "no"));
+// console.log(fightModule.fight());
+
+// /*
+// CommonJS and AMD (asynchronous module definition)
+// this is what I'm familiar with through node
+// */
+
+// // const api = require("api");
+
+// // function someFunc() {}
+
+// // module.exports = {
+// //   someFunc,
+// // };
+
+// // Modules are meant to be loaded synchronously- which works fine in node but not ideal for browser use
+
+// // AMD looks like this:
+// // designed specifically for the browser to load scripts or modules asynchronously
+
+// // define(["module1", "module2"], function (module1Import, module2Import) {
+// //   let module1 = module1Import; //.fight;
+// //   let module2 = module2Import; // .importedFunc2
+
+// //   function dance() {
+// //     //do stuff
+// //   }
+// //   return {
+// //     dance: dance,
+// //   };
+// // });
+
+// //ES6 import/export keywords - the way I'm used to with React
