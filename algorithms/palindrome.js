6ca25ee91a2d95d5440cf4aa520d8c914c7fbb31
FreedomWriter <natalienetworks@gmail.com>
2020-10-16 10:29:01 -0500

    parallel, sequence, and race

--- algorithms/palindrome.js
+++ algorithms/palindrome.js
@@ -1,21 +1,21 @@
-console.log("PALINDROME");
+// console.log("PALINDROME");
 
-function pointerAnagram(str) {
-  if (!str.length) return "Empty String";
-  let lIdx = 0;
-  let rIdx = str.length - 1;
-  while (lIdx < rIdx) {
-    if (str[lIdx] !== str[rIdx]) {
-      return false;
-    }
-    lIdx++;
-    rIdx--;
-  }
-  return true;
-}
+// function pointerAnagram(str) {
+//   if (!str.length) return "Empty String";
+//   let lIdx = 0;
+//   let rIdx = str.length - 1;
+//   while (lIdx < rIdx) {
+//     if (str[lIdx] !== str[rIdx]) {
+//       return false;
+//     }
+//     lIdx++;
+//     rIdx--;
+//   }
+//   return true;
+// }
 
-console.log(pointerAnagram("azbza"));
-console.log(pointerAnagram("azbaza"));
-console.log(pointerAnagram("azbsaz"));
-console.log(pointerAnagram("q"));
-console.log(pointerAnagram(""));
+// // console.log(pointerAnagram("azbza"));
+// // console.log(pointerAnagram("azbaza"));
+// // console.log(pointerAnagram("azbsaz"));
+// // console.log(pointerAnagram("q"));
+// // console.log(pointerAnagram(""));
