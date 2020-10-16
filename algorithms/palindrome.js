835f37c6e4340ff16f75564eed50d0b2761a0f3b
FreedomWriter <natalienetworks@gmail.com>
2020-10-15 21:12:29 -0500

    palindrome

---
+++ algorithms/palindrome.js
@@ -0,0 +1,21 @@
+console.log("PALINDROME");
+
+function pointerAnagram(str) {
+  if (!str.length) return "Empty String";
+  let lIdx = 0;
+  let rIdx = str.length - 1;
+  while (lIdx < rIdx) {
+    if (str[lIdx] !== str[rIdx]) {
+      return false;
+    }
+    lIdx++;
+    rIdx--;
+  }
+  return true;
+}
+
+console.log(pointerAnagram("azbza"));
+console.log(pointerAnagram("azbaza"));
+console.log(pointerAnagram("azbsaz"));
+console.log(pointerAnagram("q"));
+console.log(pointerAnagram(""));
