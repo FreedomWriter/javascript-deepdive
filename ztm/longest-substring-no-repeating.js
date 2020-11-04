c89cef2a11b65e6ca6b9292942ea9a685f635964
FreedomWriter <natalienetworks@gmail.com>
2020-11-04 14:10:22 -0600

    palindrome wip

--- ztm/longest-substring-no-repeating.js
+++ ztm/longest-substring-no-repeating.js
@@ -1,121 +1,120 @@
-console.log("LONGEST SUBSTRING WITH NO REPEATING CHARACTERS");
+// console.log("LONGEST SUBSTRING WITH NO REPEATING CHARACTERS");
 
-/*
-Given a string, find the length of the longest substring without repeating characters
+// /*
+// Given a string, find the length of the longest substring without repeating characters
 
-STEP ONE - VERIFY THE CONSTRAINTS
----------------------------------
+// STEP ONE - VERIFY THE CONSTRAINTS
+// ---------------------------------
 
-- Is the string contiguous? Yes, look for a substring vs a subsequence
-    - contiguous means: the characters are sequential with no breaks in between them
+// - Is the string contiguous? Yes, look for a substring vs a subsequence
+//     - contiguous means: the characters are sequential with no breaks in between them
 
-- Does case senstivity matter? No, assume lowercase
+// - Does case senstivity matter? No, assume lowercase
 
-STEP TWO - WRITE OUT SOME TEST CASES
-------------------------------------
- "abccabb" --> 3
- - Two substrings, both have length of 3
- 
- "ccccccc" --> 1
+// STEP TWO - WRITE OUT SOME TEST CASES
+// ------------------------------------
+//  "abccabb" --> 3
+//  - Two substrings, both have length of 3
 
- "" --> 0
+//  "ccccccc" --> 1
 
- "abcbda" --> 4
+//  "" --> 0
 
-STEP THREE - SOLVE WITHOUT CODE
--------------------------------
+//  "abcbda" --> 4
 
-- iterate through string
-- if while cur_char != next_char
-    - increment longest string count
-    - move second pointer
+// STEP THREE - SOLVE WITHOUT CODE
+// -------------------------------
 
+// - iterate through string
+// - if while cur_char != next_char
+//     - increment longest string count
+//     - move second pointer
 
+// */
 
-*/
+// function findLongestSubstringNoRepeating(s) {
+//   let longestLength = 0;
 
-function findLongestSubstringNoRepeating(s) {
-  let longestLength = 0;
+//   if (s.length <= 1) {
+//     return s.length;
+//   }
 
-  if (s.length <= 1) {
-    return s.length;
-  }
+//   for (let left = 0; left < s.length; left++) {
+//     let seenChars = {},
+//       curLongest = 0;
 
-  for (let left = 0; left < s.length; left++) {
-    let seenChars = {},
-      curLongest = 0;
+//     for (let right = left; right < s.length; right++) {
+//       const curChar = s[right];
 
-    for (let right = left; right < s.length; right++) {
-      const curChar = s[right];
+//       if (!seenChars[curChar]) {
+//         curLongest++;
+//         seenChars[curChar] = true;
+//         if (longestLength < curLongest) {
+//           longestLength = curLongest;
+//         }
+//       } else {
+//         break;
+//       }
+//     }
+//   }
+//   return longestLength;
+// }
 
-      if (!seenChars[curChar]) {
-        curLongest++;
-        seenChars[curChar] = true;
-        if (longestLength < curLongest) {
-          longestLength = curLongest;
-        }
-      } else {
-        break;
-      }
-    }
-  }
-  return longestLength;
-}
+// // console.log(findLongestSubstringNoRepeating("abccabb"));
+// // console.log(findLongestSubstringNoRepeating("ccccccc"));
+// // console.log(findLongestSubstringNoRepeating("abcbda"));
+// // console.log(findLongestSubstringNoRepeating("a"));
+// // console.log(findLongestSubstringNoRepeating(""));
+// // console.log(findLongestSubstringNoRepeating("pwwkew"));
 
-// console.log(findLongestSubstringNoRepeating("abccabb"));
-// console.log(findLongestSubstringNoRepeating("ccccccc"));
-// console.log(findLongestSubstringNoRepeating("abcbda"));
-// console.log(findLongestSubstringNoRepeating("a"));
-// console.log(findLongestSubstringNoRepeating(""));
-// console.log(findLongestSubstringNoRepeating("pwwkew"));
+// function findLongestSubstringNoRepeatingOptimized(s) {
+//   if (s.length <= 1) return s.length;
+//   const seenChars = {};
+//   let left = 0,
+//     longest = 0;
+//   for (let right = 0; right < s.length; right++) {
+//     // const curChar = s[right];
+//     // const prevSeen = seenChars[s[right]];
 
-function findLongestSubstringNoRepeatingOptimized(s) {
-  if (s.length <= 1) return s.length;
-  const seenChars = {};
-  let left = 0,
-    longest = 0;
-  for (let right = 0; right < s.length; right++) {
-    // const curChar = s[right];
-    // const prevSeen = seenChars[s[right]];
+//     if (seenChars[s[right]] >= left) {
+//       left = seenChars[s[right]] + 1;
+//     }
+//     seenChars[s[right]] = right;
+//     // adding a 1 because left and right are indexes, so off by 1
+//     if (longest < right - left + 1) {
+//       longest = right - left + 1;
+//     }
+//   }
+//   return longest;
+// }
 
-    if (seenChars[s[right]] >= left) {
-      left = seenChars[s[right]] + 1;
-    }
-    seenChars[s[right]] = right;
-    // adding a 1 because left and right are indexes, so off by 1
-    if (longest < right - left + 1) {
-      longest = right - left + 1;
-    }
-  }
-  return longest;
-}
+// // console.log(findLongestSubstringNoRepeatingOptimized("abccabb"));
+// // console.log(findLongestSubstringNoRepeatingOptimized("ccccccc"));
+// // console.log(findLongestSubstringNoRepeatingOptimized("abcbda"));
+// // console.log(findLongestSubstringNoRepeatingOptimized("a"));
+// // console.log(findLongestSubstringNoRepeatingOptimized(""));
+// console.log(findLongestSubstringNoRepeatingOptimized("pwwkew"));
 
-// console.log(findLongestSubstringNoRepeatingOptimized("abccabb"));
-// console.log(findLongestSubstringNoRepeatingOptimized("ccccccc"));
-// console.log(findLongestSubstringNoRepeatingOptimized("abcbda"));
-// console.log(findLongestSubstringNoRepeatingOptimized("a"));
-// console.log(findLongestSubstringNoRepeatingOptimized(""));
-console.log(findLongestSubstringNoRepeatingOptimized("pwwkew"));
+// //USING MAP
 
-//USING MAP
+// function findLongestSubstringNoRepeatingOptimized(s) {
+//   if (s.length <= 1) return s.length;
+//   const seenChars = new Map();
+//   let left = 0,
+//     longest = 0;
+//   for (let right = 0; right < s.length; right++) {
+//     const curChar = s[right];
+//     // if seenChars does not have curChar, prevSeen will be assigned "undefined"
+//     const prevSeen = seenChars.get(curChar);
 
-function findLongestSubstringNoRepeatingOptimized(s) {
-  if (s.length <= 1) return s.length;
-  const seenChars = new Map();
-  let left = 0,
-    longest = 0;
-  for (let right = 0; right < s.length; right++) {
-    const curChar = s[right];
-    const prevSeen = seenChars.get(curChar);
+//     if (prevSeen >= left) {
+//       left = prevSeen + 1;
+//     }
+//     seenChars.set(curChar, right);
 
-    if (prevSeen >= left) {
-      left = prevSeen + 1;
-    }
-    seenChars.set(curChar, right);
-
-    if (longest < right - left + 1) {
-      longest = right - left + 1;
-    }
-  }
-  return longest;
-}
+//     if (longest < right - left + 1) {
+//       longest = right - left + 1;
+//     }
+//   }
+//   return longest;
+// }
---
+++ ztm/valid-palindrome-and-almost-palindrome.js
@@ -0,0 +1,20 @@
+console.log("VALID PALINDROME AND ALMOST PALINDROME");
+
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring case sensitivity.
+
+STEP ONE - VERIFY THE CONSTRAINTS
+---------------------------------
+
+
+
+STEP TWO - WRITE OUT SOME TEST CASES
+------------------------------------
+
+
+STEP THREE - SOLVE WITHOUT CODE
+-------------------------------
+
+
+
+*/
