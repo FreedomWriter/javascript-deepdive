ecfea24b24683f85d45468c6140c4fb06159993f
FreedomWriter <natalienetworks@gmail.com>
2020-11-16 13:07:01 -0600

    colts binary

--- algorithms/colt/binarySearch.js
+++ algorithms/colt/binarySearch.js
@@ -20,3 +20,26 @@ function binarySearch(arr, el) {
 }
 
 console.log(binarySearch([2, 5, 6, 9, 13, 15, 28], 15));
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28], 115));
+
+function coltsBinarySearch(arr, el) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== el && start <= end) {
+    if (el < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === el) {
+    return middle;
+  }
+  return -1;
+}
+
+console.log(coltsBinarySearch([2, 5, 6, 9, 13, 15, 28], 15));
+console.log(coltsBinarySearch([2, 5, 6, 9, 13, 15, 28], 155));
